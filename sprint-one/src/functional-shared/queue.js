02096aa60d9ce1ccb14dcf3a766907180f2417fd
francoabaroa <franco9395@gmail.com>
2016-09-14 16:30:23 -0700

    test for returning correct size passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.counter = 0;
+  _.extend(queue, queueMethods);
+  return queue;
 };
 
 var queueMethods = {};
+
+// queueMethods.enqueue = function (value) {
+//   console.log(queue);
+//   this.storage[this.counter] = value;
+//   this.counter++;
+// };
+
+// queueMethods.dequeue = function () {
+//   this.counter--;
+//   if (this.counter > 0) {
+//     var dequeued = this.storage[0];
+//     this.storage[0] = this.storage[counter];
+//   }
+//   return dequeued;
+// };
+
+queueMethods.size = function () {
+  return this.counter;
+};
 
 
