18afee0ac2dd885488b85fc56de51168affbc809
francoabaroa <franco9395@gmail.com>
2016-09-15 10:48:12 -0700

    Reports a size of zero for a new queue test passed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(Queue.prototype);
+  queue.counter = 0;
+  return queue;
+
 };
 
 var queueMethods = {};
 
-
+Queue.prototype.size = function () {
+  return this.counter;
+};
