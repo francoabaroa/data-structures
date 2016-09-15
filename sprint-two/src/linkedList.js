96ae4de2033d5b6a03ca6b646c98776412ee1624
francoabaroa <franco9395@gmail.com>
2016-09-15 15:37:53 -0700

    designating a new tail when nodes are added test passed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,9 +1,21 @@
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      list.tail = Node(value);
+    }
   };
 
   list.removeHead = function() {
@@ -15,14 +27,6 @@ var LinkedList = function() {
   return list;
 };
 
-var Node = function(value) {
-  var node = {};
-
-  node.value = value;
-  node.next = null;
-
-  return node;
-};
 
 /*
  * Complexity: What is the time complexity of the above functions?
