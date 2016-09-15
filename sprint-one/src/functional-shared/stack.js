5c6af0908601374fa3a1da5825d3b9fc65ed368c
francoabaroa <franco9395@gmail.com>
2016-09-14 20:07:46 -0700

    Reports a size of zero for a new stack, test passed

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  _.extend(storage, stackMethods);
+  storage.counter = 0;
+  return storage;
+
 };
 
 var stackMethods = {};
 
 
+stackMethods.size = function () {
+  return this.counter;
+};
