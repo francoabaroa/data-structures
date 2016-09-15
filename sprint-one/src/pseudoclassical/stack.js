5b1550a15ad048b5c28d77a414e677ffa95f878d
francoabaroa <franco9395@gmail.com>
2016-09-15 14:42:13 -0700

    Reports a size of zero with a new stack test passed

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,9 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
 };
 
-
+Stack.prototype.size = function () {
+  return this.counter;
+};
