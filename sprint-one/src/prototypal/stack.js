7288ce454b8478b0881c8c812bb7567dbdc112b6
francoabaroa <franco9395@gmail.com>
2016-09-15 14:33:03 -0700

    extending the constructor function's prototype test passed and all tests pass

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,26 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var stack = Object.create(Stack.prototype);
+  var stack = Object.create(stackMethods);
   stack.counter = 0;
-  console.log(stack instanceof Stack);
   return stack;
 };
 
-var stackMethods = Object.create(Stack);
 
-// Stack.prototype.constructor = Stack.prototype;
-console.log(stackMethods.prototype.constructor);
+var stackMethods = {};
 
-Stack.prototype.size = function () {
+stackMethods.size = function () {
   return this.counter;
 };
 
-Stack.prototype.push = function (value) {
+stackMethods.push = function (value) {
   this[this.counter++] = value;
 };
 
-Stack.prototype.pop = function () {
+stackMethods.pop = function () {
   if (this.counter > 0) {
     this.counter--;
     var temp = this[this.counter];
