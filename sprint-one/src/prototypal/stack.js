0ce7ad509486d0b7af7cb7205d49cddd15e74cfb
francoabaroa <franco9395@gmail.com>
2016-09-15 10:05:30 -0700

    reporting a size of zero passing!

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(Stack.prototype);
+  stack.counter = 0;
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = Object.create(Stack.prototype);
 
 
+Stack.prototype.size = function () {
+  return this.counter;
+};
+
+console.log(stackMethods.size);
