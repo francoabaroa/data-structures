adf45dee3e59823694dfa724c8a8dd0854b0c959
francoabaroa <franco9395@gmail.com>
2016-09-16 11:29:38 -0700

    Refactored code and added time complexity.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -8,21 +8,17 @@ var setPrototype = {};
 
 setPrototype.add = function(item) {
   this._storage [ item ] = item;
-    // console.log('success', this._storage );
-  
 };
 
 setPrototype.contains = function(item) {
-  if ( this._storage[item] === item) {
-    return true;
-  }
-  return false;
+  return this._storage [ item ] === item;
 };
 
 setPrototype.remove = function(item) {
-  this._storage[item] = undefined;
+  this._storage [ item ] = undefined;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  Time complexity for the above methods is O(1).
  */
