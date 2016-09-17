c7b98228e61f331abbad55c3d9bfeb71cee1d996
francoabaroa <franco9395@gmail.com>
2016-09-17 10:17:07 -0700

    should insert values at the correct location in the tree test passing

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,20 +1,31 @@
 var BinarySearchTree = function(value) {
   var biTree = {};
   _.extend(biTree, biTreeMethods);
+  // console.log(biTree);
   biTree.value = value;
-  biTree.left;
-  biTree.right;
-
+  biTree.left = undefined;
+  biTree.right = undefined;
+  
   return biTree;
 };
 
 var biTreeMethods = {};
 
 biTreeMethods.insert = function (value) {
-  // this.
-  // if (value > this.value) {
-
-  // }
+  if (value < this.value) {
+    if (this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+  console.log(this);
 };
 
 biTreeMethods.contains = function () {
