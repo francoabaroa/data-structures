09085bb5e38d179c88f6a9e2b9c8497fd4304d23
francoabaroa <franco9395@gmail.com>
2016-09-16 21:05:13 -0700

    all tests for graph passing

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -21,39 +21,56 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var removed;
+  if (!this.edges[node]) {
+    this.edges[node] = [];
+  }
+  if (this.edges[node].indexOf(node) === 0) {
+    removed = this.edges[node][1];  
+  } else if (this.edges[node].indexOf(node) === 1) {
+    removed = this.edges[node][0];
+  }
   if (this.nodes[node] === node) {
     this.nodes[node] = undefined;
-    // this.edges[node] = undefined;
-    for (var key in this.edges) {
-      key = undefined;
-    }
+    this.removeEdge(node, removed);
   }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  if (this.edges[toNode] === fromNode || this.edges[fromNode] === toNode) {
-    return true;
+  if (this.edges[fromNode] !== undefined) {
+    return this.edges[fromNode].includes(fromNode) && this.edges[fromNode].includes(toNode);
+  } else if (this.edges[toNode] !== undefined) {
+    return this.edges[toNode].includes(fromNode) && this.edges[toNode].includes(toNode);
+  } else {
+    return false;
   }
-  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.edges[toNode] = fromNode;
-  this.edges[fromNode] = toNode;
+  this.edges[fromNode] = [fromNode, toNode];
+  this.edges[toNode] = [fromNode, toNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  this.edges[toNode] = undefined;
   this.edges[fromNode] = undefined;
+  this.edges[toNode] = undefined;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.nodes) {
+    console.log(key);
+    cb(this.nodes[key]);
+  }
+
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
