7d0b07ec1f9d005f33250576d44d6726410bdb54
francoabaroa <franco9395@gmail.com>
2016-09-16 21:57:07 -0700

    should have methods named insert, contains and depthFirstLog test passed

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,25 @@
 var BinarySearchTree = function(value) {
+  var biTree = {};
+  _.extend(biTree, biTreeMethods);
+  biTree.left;
+  biTree.right;
+
+  return biTree;
 };
 
+var biTreeMethods = {};
+
+biTreeMethods.insert = function () {
+
+};
+
+biTreeMethods.contains = function () {
+
+};
+
+biTreeMethods.depthFirstLog = function () {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
