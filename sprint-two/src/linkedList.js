c9928e9cb91da3dcd84c68853052ea706a8654df
Franco Abaroa <franco9395@gmail.com>
2016-09-25 22:15:36 -0700

    re-did LinkedList.. all tests passing

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,55 +1,42 @@
-var Node = function(value) {
-  var node = {};
-
-  node.value = value;
-  node.next = null;
-
-  return node;
-};
-
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
-
   list.addToTail = function(value) {
     if (list.head === null) {
       list.head = Node(value);
     }
-    while (!list.head.next) {
-      list.head.next = list.head;
-    }
-    list.head.next = list.tail = Node(value);
+    list.tail = list.head.next = Node(value);
+    return list;
   };
-
 
   list.removeHead = function() {
+    var temp = list.head;
     list.head = list.head.next;
-    return list.head.value;
+    return temp.value;
   };
 
-
   list.contains = function(target) {
-    var current = list.head.next;
-
     if (list.head.value === target) {
       return true;
-    } 
-    while (current !== null) {
-      if (current.value === target) {
-        return true;
-      }
-      current = current.next;
+    }
+    if (list.tail.value === target) {
+      return true;
     }
     return false;
   };
-
   return list;
 };
 
+var Node = function(value) {
+  var node = {};
+  node.value = value;
+  node.next = null;
+  return node;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
-
+ The time complexity for LinkedList is O(n);
  */
