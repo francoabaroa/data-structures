446045c6a2ad3ebd7f689912744b7b5129b2df40
Franco Abaroa <franco9395@gmail.com>
2016-09-28 23:05:54 -0700

    Refactoring

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,67 +1,40 @@
-
-
-// Instantiate a new graph
 var Graph = function() {
   this.nodes = {};
   this.edges = {};
 };
 
-// Add a node to the graph, passing in the node's value.
+
 Graph.prototype.addNode = function(node) {
   this.nodes[node] = node;
 };
 
-// Return a boolean value indicating if the value passed to contains is represented in the graph.
+
 Graph.prototype.contains = function(node) {
-  if (this.nodes[node] === node) {
-    return true;
-  }
-  return false;
+  return (this.nodes[node] === node) ? true : false;
 };
 
-// Removes a node from the graph.
+
 Graph.prototype.removeNode = function(node) {
-  //Adding a removed variable because removeNode calls on the removeEdge method, which takes two arguments
-  var removed;
-  if (!this.edges[node]) {
-    this.edges[node] = [];
-  }
-  //This if/else if control flow statement is used to give the removed variable its value
-  if (this.edges[node].indexOf(node) === 0) {
-    removed = this.edges[node][1];  
-  } else if (this.edges[node].indexOf(node) === 1) {
-    removed = this.edges[node][0];
-  }
-  //This if statement is used to set the target node = undefined and then call removeEdge method with node & removed
-  if (this.nodes[node] === node) {
-    this.nodes[node] = undefined;
-    this.removeEdge(node, removed);
-  }
+  this.nodes[node] = null;
+
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  //The if statement checks if there is a hadEdge array at edges[fromNode] & then checks if that array includes fromNode and toNode
-  //The else if statement checks if there is a hadEdge array at edges[toNode] & then checks if that array includes fromNode and toNode
-  if (this.edges[fromNode] !== undefined) {
-    return this.edges[fromNode].includes(fromNode) && this.edges[fromNode].includes(toNode);
-  } else if (this.edges[toNode] !== undefined) {
-    return this.edges[toNode].includes(fromNode) && this.edges[toNode].includes(toNode);
-  } else {
+  if (this.nodes[fromNode] === null || this.nodes[toNode] === null) {
     return false;
   }
+  return (this.edges[JSON.stringify([fromNode, toNode].sort())]) ? true : false;
 };
 
-// Connects two nodes in a graph by adding an edge between them.
+
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.edges[fromNode] = [fromNode, toNode];
-  this.edges[toNode] = [fromNode, toNode];
+  this.edges[JSON.stringify([fromNode, toNode].sort())] = [fromNode, toNode];
 };
 
-// Remove an edge between any two specified (by value) nodes.
+
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  this.edges[fromNode] = undefined;
-  this.edges[toNode] = undefined;
+  delete this.edges[JSON.stringify([fromNode, toNode].sort())];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here'
-  _.extend(newTree, treeMethods);
-  newTree.children = [];  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+  newTree.children = [];
 
   return newTree;
 };
@@ -16,16 +15,43 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  for (var i = 0; i < this.children.length; i++) {
-    if (this.children[i].value === target || this.children[i].contains(target)) {
-      return true;
-    } 
+  var contains = false;
+
+  function recurse (tree) {
+    if (tree.children.length === 0) {
+      return;
+    }
+
+    if (tree.value === target) {
+      contains = true;
+    }
+
+    for (var i = 0; i < tree.children.length; i++) {
+      console.log(tree.children[i].value);
+      debugger;
+     if (tree.children[i].value === target) {
+        contains = true;
+        console.log('in hurr');
+      } else {
+        recurse(tree.children[i]);
+      }
+      console.log(tree.children[i], 'here');
+    }
   }
-  return false;
+  recurse(this);
+  return contains;
 };
+
+var tree = Tree(4);
+tree.addChild(5);
+tree.addChild(6);
+tree.children[0].addChild(7);
+tree.children[1].addChild(8);
+console.log(tree.contains(5));
+// console.log(tree);
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
-Time complexity for tree is O(n).
  */
